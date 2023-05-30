3730a148ceed45c4352a8d96424fb05b7f63fa4e
mdvitalie.ie <mdvitalie@gmail.com>
2023-05-30 18:19:01 +0100

    all dependencies have been installed

---
+++ screens/AllExpenses.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AllExpenses = () => {
+  return (
+    <View>
+      <Text>All Expenses Screen</Text>
+    </View>
+  )
+}
+
+export default AllExpenses
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RecentExpenses = () => {
+  return (
+    <View>
+      <Text>Recent Expenses Screen</Text>
+    </View>
+  )
+}
+
+export default RecentExpenses
